9e3f1835c6957586e579ec1648ab5111820da4c4
Ekawat40 <aekwatt@hotmail.com>
2018-09-08 00:17:59 +0700

    Initial commit

---
+++ app/src/main/java/helloworld/ekawat/com/traveler/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package helloworld.ekawat.com.traveler.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import helloworld.ekawat.com.traveler.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
